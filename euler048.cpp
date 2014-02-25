c9fee1358c9824bdb0b7a41db8e1f56c4935a518
Charles Pigott <charlespigott@googlemail.com>
2014-02-25 22:16:59 +0000

    Add 048

---
+++ euler048.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <gmp.h>
+#include <gmpxx.h>
+
+
+int main()
+{
+	mpz_class sum = 0;
+	for (int i = 1; i <= 1000; i++) {
+		mpz_class pow;
+		mpz_ui_pow_ui(pow.get_mpz_t(), i, i);
+		sum += pow;
+	}
+	std::cout << "Sum: " << sum << std::endl;
+}
