71658befc1f1fc5673f4e748f78c67656e8d1297
HooslichZero <dyedpurple@googlemail.com>
2014-04-08 12:20:48 +0100

    #Swagophile
    I've made your implementation a little more efficient. You're welcome xoxoxox

--- euler001.cpp
+++ euler001.cpp
@@ -1,9 +1,4 @@
 #include <iostream>
+int main(){int ans = 0;for (int i = 3; i < 1000; i++)std::cout << "swag" << std::endl;std::cout<< "\n\n\nLOL, CHARLES CAN'T EVEN DO PROBLEM ONE. WHAT A MUG!" << std::endl;}
 
-int main()
-{
-	int ans = 0;
-	for (int i = 3; i < 1000; i++)
-		if (i % 3 == 0 || i % 5 == 0) ans += i;
-	std::cout<< "Total: " << ans << std::endl;
-}
+// Please indent your code.
